69dbdcbf293dc0ff7345897d76c054dd9b40aedd
Jidro.Z <z3069566588@163.com>
2023-04-24 10:33:38 +0800

    Added some fcitx configurations.

--- system_configuration/profile.d/fcitx.sh
+++ system_configuration/profile.d/fcitx.sh
@@ -1,14 +1,23 @@
+# Setting Chinese locales.
+#export LANG="zh_CN.UTF-8"
+
 # Fcitx Setting
-#export LANG="zh_CN.UTF-8" # Setting Chinese locales.
+##export GLFW_IM_MODULE=ibus
+#export GLFW_IM_MODULE=fcitx
+#export XMODIFIERS=@im=fcitx
+#export GTK_IM_MODULE=fcitx
+#export SDL_IM_MODULE=fcitx
+#export INPUT_METHOD=fcitx
+#export QT_IM_MODULE=fcitx
 #export XIM_PROGRAM=fcitx
 #export XIM=fcitx
-#export GTK_IM_MODULE=fcitx
-#export QT_IM_MODULE=fcitx
-#export XMODIFIERS=@im=fcitx
 
-GTK_IM_MODULE=fcitx
-QT_IM_MODULE=fcitx
+#export GLFW_IM_MODULE=ibus
+GLFW_IM_MODULE=fcitx
 XMODIFIERS=@im=fcitx
-INPUT_METHOD=fcitx
+GTK_IM_MODULE=fcitx
 SDL_IM_MODULE=fcitx
-GLFW_IM_MODULE=ibus
+INPUT_METHOD=fcitx
+QT_IM_MODULE=fcitx
+XIM_PROGRAM=fcitx
+XIM=fcitx
